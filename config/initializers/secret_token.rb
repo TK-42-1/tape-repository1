65a92a00d6993e62240ded7fbce0c0e8afe47339
TK-42-1 <mister.teekay@gmail.com>
2013-03-18 15:10:20 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TapeRepository::Application.config.secret_token = 'd928382bf84a881800559368d5de2ebe17fd66eb72e92d261870b47bfd61b11a05873baa935775636cff0e5c02a3ccd095c981aa425d795c2dafb03f5b32aeab'
